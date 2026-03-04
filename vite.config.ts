21455e031aa13964d2ed1c38c4ac554c93e6f6e5
Afek <afekaharoni@gmail.com>
2026-03-04 21:03:02 +0200

    bug fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/afekaharoni.github.io/",  
   server: {
     host: "::",
     port: 8080,
